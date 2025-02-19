91832d1a6945afd0844271a983a6a888d51c1367
dkulik <kulikdenis1996x@gmail.com>
2025-02-19 21:05:51 +0700

    feat: add home section

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
 import './app.css'
+import Home from './components/Home.svelte'
 </script>
 
 <main class="w-screen h-screen flex justify-center bg-amber-50">
-  <div class="container pt-20 p-4"></div>
+  <div class="container pt-20 p-4">
+    <Home />
+  </div>
 </main>
 
 <style>
---
+++ src/components/Home.svelte
@@ -0,0 +1,28 @@
+<script>
+import denisPhoto from '../assets/denis.webp'
+import annaPhoto from '../assets/anna.webp'
+</script>
+
+<section>
+  <div class="flex items-center justify-center mb-32">
+    <div
+      class="bg-white p-4 shadow-lg rounded-lg border w-52 transform -rotate-6"
+    >
+      <img src={denisPhoto} alt="Бота" class="w-full h-auto rounded-lg" />
+      <p class="text-center text-lg font-handwriting mt-2">Денис</p>
+    </div>
+
+    <div
+      class="bg-white p-4 shadow-lg rounded-lg border w-52 transform rotate-6 translate-y-20"
+    >
+      <img src={annaPhoto} alt="Аленка" class="w-full h-auto rounded-lg" />
+      <p class="text-center text-lg font-handwriting mt-2">Анечка</p>
+    </div>
+  </div>
+
+  <h2 class="text-center text-3xl font-handwriting">Узнаете этих малышей?</h2>
+  <p class="text-center">
+    Да-да, это мы!<br /> Мы рады пригласить Вас на торжество по случаю нашей свадьбы
+    и разделить этот счастливый момент с нами
+  </p>
+</section>
