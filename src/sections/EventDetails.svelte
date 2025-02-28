1c5df6f78796cb6f46f15b288ddbfd4b8d92beca
dkulik <kulikdenis1996x@gmail.com>
2025-02-28 12:21:15 +0700

    feat: - add gh-actions deploy - update icons

--- src/sections/EventDetails.svelte
+++ src/sections/EventDetails.svelte
@@ -4,6 +4,8 @@ import { inview } from 'svelte-inview'
 import Heading from '../components/Heading.svelte'
 import Calendar from '../components/Calendar.svelte'
 import Container from '../components/Container.svelte'
+import time from '../assets/time.png'
+import date from '../assets/calendar.png'
 
 let inView = false
 </script>
@@ -16,11 +18,16 @@ let inView = false
   <AnimatedContainer {inView}>
     <Container>
       <Heading>Дата и время проведения</Heading>
-      <p class="text-2xl font-bold text-gray-800 mb-2">
-        <span>18 августа 2025</span>
-        <br />
-        <span>15:00</span>
-      </p>
+      <div class="text-2xl font-bold text-gray-800 mb-2">
+        <div class="flex justify-center items-center gap-2">
+          <img src={date} alt="date" class="w-6" />
+          <span>18 августа 2025</span>
+        </div>
+        <div class="flex justify-center items-center gap-2">
+          <img src={time} alt="time" class="w-6" />
+          <span>15:00</span>
+        </div>
+      </div>
       <Calendar selectedDay={18} firstDayOffset={4} daysInMonth={31} />
     </Container>
   </AnimatedContainer>
